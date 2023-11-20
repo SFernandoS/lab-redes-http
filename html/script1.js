4d2fe233ce424036956544eeab6c1ce7c31722d4
Fernando Vargas <nandovargas7@gmail.com>
2023-11-20 16:06:08 -0300

    Melhoramente

---
+++ html/script1.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('Script 1 carregado!');
+});
---
+++ html/script2.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('Script 2 carregado!');
+});
